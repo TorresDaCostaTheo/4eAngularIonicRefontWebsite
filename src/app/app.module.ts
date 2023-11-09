00b43b5210e5eb7a1089f931a791355d6e820db8
TorresDaCostaTheo <newtete789@gmail.com>
2023-11-09 10:35:03 +0100

    list products functional, require some ui to it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,SharedModule],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    SharedModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
-  exports:[]
+  exports: [],
 })
 export class AppModule {}
--- src/app/list-product/list-product.page.ts
+++ src/app/list-product/list-product.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/productModel';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-list-product',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-product.page.scss'],
 })
 export class ListProductPage implements OnInit {
-
-  constructor() { }
+  listProducts: Product[] = [];
+  category: number = 0;
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-  }
+    let listProductsTemporary: Product[];
 
+    this.route.queryParams.subscribe((params) => {
+      if (this.router.getCurrentNavigation()?.extras.state) {
+        this.category =
+          this.router.getCurrentNavigation()?.extras.state?.['category'];
+        this.getByCategory(this.category);
+      }
+    });
+  }
+  getByCategory(category: number) {
+    this.listProducts = [];
+    let listProductsTemporary: Product[];
+    this.productService.getProducts().subscribe(
+      (res) => {
+        listProductsTemporary = res;
+        listProductsTemporary.forEach((product) => {
+          if (product.category === category) {
+            console.log(product);
+            console.table(this.listProducts);
+            this.listProducts.push(product);
+          }
+        });
+      },
+      (err) => {
+        console.log('error');
+      }
+    );
+  }
 }
