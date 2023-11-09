8fc17e9ae8c48696484d40b65a62529f273fab5c
VivienS5 <sobra.vivien@gmail.com>
2023-11-09 12:31:38 +0100

    Fix appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
-import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    Pagebateaux
     BrowserModule,
     HttpClientModule,
     IonicModule.forRoot(),
