04ae238cbf08761ef234c425ac1d40d34b2ddb07
Guillaume SIMON <guillaumesimon597@gmail.com>
2023-11-21 11:42:51 +0100

    Implement user action confirmation feature

--- src/app/panier/panier.page.ts
+++ src/app/panier/panier.page.ts
@@ -2,36 +2,44 @@ import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import {Produit} from '../models/produit';
 import { PanierService } from '../panierService/panier.service';
+import { HttpClient } from '@angular/common/http';
+import { Restaurant } from '../models/restaurant';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-panier',
   templateUrl: './panier.page.html',
   styleUrls: ['./panier.page.scss'],
 })
-export class PanierPage implements OnInit,OnChanges {
-  restaurantName:{id:number,address:string}[] = Array(4).fill({id:0,address:"Jean jaures"});
+export class PanierPage implements OnInit {
+  restaurants:Restaurant[] = this.panierService.restaurants;
   produitsCart:Produit[] = this.panierService.produits;
   selectedValue:string = "-1"
   sum:number = 0;
 
-  constructor(private router:Router,private panierService:PanierService) { }
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes);
-    this.sumTotal()
+  constructor(private router:Router,private panierService:PanierService,private alertController:AlertController) {
   }
-
   /**
    * Initialise le composant
    */
   ngOnInit() {
-    this.sumTotal()
+    this.panierService.callback = ()=>{
+      this.sumTotal()
+    }
   }
   /**
    * Envoie les produits au panier
    */
-  submitProduit(){
-    this.panierService.submitProduit(this.produitsCart.map((produit:Produit)=>produit.shortProduit))
-    .then((result)=>{console.log(result)}).catch((error)=>console.error(error));
+  async submitProduit(){
+   return this.panierService.submitProduit(this.produitsCart.map((produit:Produit)=>produit.shortProduit))
+    .then((result)=>{
+      console.log(result);
+      return Promise.resolve()
+     })
+     .catch((error)=>{
+        console.log(error);
+        return Promise.reject()
+     })
   }
   /**
    *  Supprime le produit du panier
@@ -42,6 +50,35 @@ export class PanierPage implements OnInit,OnChanges {
     if(result){
       this.sumTotal()
     }
+  }
+  deleteAllProduit(){
+    this.produitsCart = [];
+    this.sumTotal()
+  }
+  async confirmOrder(){
+    const alert = await this.alertController.create({
+      header: "Confirmation du panier",
+      message: "Voulez-vous confirmer l'achat ?\n Cette action est irréversible",
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel',
+          cssClass: 'danger',
+        },
+        {
+          text: 'Confirmer',
+          role: 'confirm',
+          cssClass: 'primary',
+          handler: () => {
+            this.submitProduit().then(()=>{
+              console.log("Commande valider")
+            })
+          }
+        }
+      ]
+    })
+    await alert.present();
+
   }
   /**
    *  Donne le restaurant sélectionner
--- src/app/panierService/panier.service.ts
+++ src/app/panierService/panier.service.ts
@@ -3,6 +3,8 @@ import { Storage } from '@ionic/storage-angular';
 import { Produit } from '../models/produit';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/productModel';
+import { Restaurant } from '../models/restaurant';
+import { RestaurantsService } from '../services/restaurants.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +12,20 @@ import { Product } from '../models/productModel';
 export class PanierService {
   private _storage: Storage | null = null;
   private _produits:Produit[]=[];
-  constructor(private storage:Storage,private productService:ProductService) {this.init() }
+  private _restaurants:Restaurant[]=[];
+  private _callbackProduct:()=>void = ()=>{};
+  constructor(private storage:Storage,private productService:ProductService,private restaurantService:RestaurantsService) {this.init() }
   /**
    * Initialise le stockage local
    */
   async init(){
     this._storage =await this.storage.create();
     this._storage.set('produitsCart',[]);
+
+
+  }
+  set callback(callback:()=>void){
+    this._callbackProduct = callback;
   }
   set produit(cartProduct:{
     idProduit:number,
@@ -27,8 +36,32 @@ export class PanierService {
     console.log(product);
     if(product){
       this._produits.push(new Produit(product.id,product.name,product.price,cartProduct.quantity));
+      this._callbackProduct();
     }
    })
+  }
+  requestRestaurants(){
+    this.restaurantService.getRestaurants().subscribe((restaurants)=>{
+      console.log(restaurants);
+      this._restaurants.push(...restaurants.map((value,i) =>{
+        const v:Restaurant = {
+          description : value.description,
+          name : value.name,
+          image : value.image,
+        }
+        return v;
+      }));
+      if(restaurants){
+        this._restaurants = restaurants;
+      }
+      else{
+        this._restaurants = [];
+      }
+    })
+    console.log(this._restaurants);
+  }
+  get restaurants():Restaurant[]{
+    return this._restaurants;
   }
   get produits():Produit[]{return this._produits}
 
@@ -48,7 +81,7 @@ export class PanierService {
       console.log(produitsCart);
       return this._storage?.set('produitsCart',produitsCart)
       .then((result)=>{
-        Promise.resolve(result)})
+        Promise.resolve("Produit ajouté au panier")})
       .catch((error)=>{Promise.reject(error)
         });
     })
