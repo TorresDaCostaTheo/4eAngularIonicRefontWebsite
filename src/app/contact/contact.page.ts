a99c413b63e3c2c308646b45005bc63ad44d8dc8
Guillaume SIMON <guillaumesimon597@gmail.com>
2023-11-21 15:34:02 +0100

    Revert "ajout toast erreur"

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 
 @Component({
@@ -7,33 +6,18 @@ import { ToastController } from '@ionic/angular';
   templateUrl: './contact.page.html',
   styleUrls: ['./contact.page.scss'],
 })
-export class ContactPage {
+export class ContactPage implements OnInit {
 
-  valErreur: string | undefined;
+  constructor(private toastController: ToastController) {}  
+  ngOnInit(){}
 
-  constructor(
-    private http: HttpClient,
-    private toastController: ToastController
-  ) { }
-
-  async afficherErreur() {
-    const pokemonId = 0; // Ici je mets 0 pour récupérer des erreurs
-
-    try {
-      const data: any = await this.http.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).toPromise();
-      console.log('Ça ne devrais pas arrivé');
-    } catch (error:any) {
-      // console.error('Erreur lors de la récupération des données:', error);
-      //this.afficherToast('Erreur lors de la récupération des données: ' + error.status);
-    }
-  }
-
-  async afficherToast(message: string) {
+  async presentToast(position:'bottom') {
     const toast = await this.toastController.create({
-      message: message,
+      message: 'Erreur 505',
       duration: 1500,
-      position: 'bottom' // Vous pouvez ajuster la position du toast selon vos besoins
+      position: position,
     });
-    toast.present();
+
+    await toast.present();
   }
 }
